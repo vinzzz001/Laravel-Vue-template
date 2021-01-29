bdf87b9adb694c4414f565eae4ad7e4e651e2f2e
Jasper Boerhof <jasperboerhof@gmail.com>
2021-01-29 12:44:27 +0100

    Added VueX and VueRouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,15 @@ import Vue from "vue";
 //Main pages
 import App from "./App.vue";
 
+//Import store
+import store from "./store/store";
+
+//Import router
+import router from "./router/index";
+
 new Vue({
     el: "#app",
+    store,
+    router,
     render: (h) => h(App),
 });
